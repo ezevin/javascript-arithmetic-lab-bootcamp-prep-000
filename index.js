36fe5306e555b2aaf93e4d668e298295d0d8ebe9
Elizabeth Zevin <ehzevin@gmail.com>
2019-01-14 16:08:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function dec(n) {
 function makeInt(n){
  var integer = parseInt(n)
  var parsed = parseInt(n, 10)
- if (isNaN(parsed)) { return true }
+ if (isNaN(parsed)) { return false }
  else {return parsed}
 }
 function preserveDecimal(n) {
