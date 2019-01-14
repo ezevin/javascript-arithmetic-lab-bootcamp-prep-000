6996c0978543c9f8730311e9cb4da2bed5d2645f
Elizabeth Zevin <ehzevin@gmail.com>
2019-01-14 15:47:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,9 +19,9 @@ function dec(n) {
   var number = n
   return n-1
 }
-function makeInt(n){
+function makeInt(n,){
   var number = n
- var a = parseInt(n, 10)
+ var a = parseInt(n,10)
 }
 function preserveDecimal(n) {
   var number = n
